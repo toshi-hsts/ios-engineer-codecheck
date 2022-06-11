0a310155e5b4b08e1ec09bccb2c10112b8fb60e4
toshi <tab.hsts@gmail.com>
2022-06-11 13:27:12 +0900

    SwiftLintの適用

--- iOSEngineerCodeCheck/ViewController2.swift
+++ iOSEngineerCodeCheck/ViewController2.swift
@@ -9,51 +9,46 @@
 import UIKit
 
 class ViewController2: UIViewController {
-    
-    @IBOutlet weak var ImgView: UIImageView!
-    
-    @IBOutlet weak var TtlLbl: UILabel!
-    
-    @IBOutlet weak var LangLbl: UILabel!
-    
-    @IBOutlet weak var StrsLbl: UILabel!
-    @IBOutlet weak var WchsLbl: UILabel!
-    @IBOutlet weak var FrksLbl: UILabel!
-    @IBOutlet weak var IsssLbl: UILabel!
-    
+
+    @IBOutlet weak var ownerAvatarImageVIew: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var languageLabel: UILabel!
+    @IBOutlet weak var starsLabel: UILabel!
+    @IBOutlet weak var watchersLabel: UILabel!
+    @IBOutlet weak var forksLabel: UILabel!
+    @IBOutlet weak var issuesLabel: UILabel!
+
     var vc1: ViewController!
-        
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         let repo = vc1.repo[vc1.idx]
-        
-        LangLbl.text = "Written in \(repo["language"] as? String ?? "")"
-        StrsLbl.text = "\(repo["stargazers_count"] as? Int ?? 0) stars"
-        WchsLbl.text = "\(repo["wachers_count"] as? Int ?? 0) watchers"
-        FrksLbl.text = "\(repo["forks_count"] as? Int ?? 0) forks"
-        IsssLbl.text = "\(repo["open_issues_count"] as? Int ?? 0) open issues"
+
+        languageLabel.text = "Written in \(repo["language"] as? String ?? "")"
+        starsLabel.text = "\(repo["stargazers_count"] as? Int ?? 0) stars"
+        watchersLabel.text = "\(repo["wachers_count"] as? Int ?? 0) watchers"
+        forksLabel.text = "\(repo["forks_count"] as? Int ?? 0) forks"
+        issuesLabel.text = "\(repo["open_issues_count"] as? Int ?? 0) open issues"
         getImage()
-        
+
     }
-    
-    func getImage(){
-        
+
+    func getImage() {
+
         let repo = vc1.repo[vc1.idx]
-        
-        TtlLbl.text = repo["full_name"] as? String
-        
+
+        titleLabel.text = repo["full_name"] as? String
+
         if let owner = repo["owner"] as? [String: Any] {
             if let imgURL = owner["avatar_url"] as? String {
-                URLSession.shared.dataTask(with: URL(string: imgURL)!) { (data, res, err) in
+                URLSession.shared.dataTask(with: URL(string: imgURL)!) { (data, _, _) in
                     let img = UIImage(data: data!)!
                     DispatchQueue.main.async {
-                        self.ImgView.image = img
+                        self.ownerAvatarImageVIew.image = img
                     }
                 }.resume()
             }
         }
-        
     }
-    
 }
